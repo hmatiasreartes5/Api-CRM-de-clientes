513bbb36431901e0852ad62312613c99c3f518cf
Hector Matias Reartes <hmatiasreartes5@hotmail.com>
2020-03-17 13:37:54 -0300

    Agregando ORM mongoose

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const routes = require('./routes')
+const routes = require('./routes');
+const mongoose = require('mongoose');
 
 //Creando el servidor 
 const app = express();
+
+//conectar a MongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/apiCRMclientes',{
+    useNewUrlParser: true
+});
 
 //Rutas de la app
 app.use('/',routes())
